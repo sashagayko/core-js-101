8a5b37f5946762e1b721c794781f68c8b7bb0f76
Sasha <hlupik69@gmail.com>
2023-01-03 02:39:08 +0300

    fix: 03-tascks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -552,8 +552,11 @@ function selectMany(/* arr, childrenSelector */) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  if (indexes.length > 1) {
+    return getElementByIndexes(arr[indexes[0]], indexes.slice(1));
+  }
+  return arr[indexes[0]];
 }
 
 
