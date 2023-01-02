94e5d7d0fb7e8264e7f8cef9335804b98ada275b
Sasha <hlupik69@gmail.com>
2023-01-03 02:55:04 +0300

    fix: 03-tascks

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -578,8 +578,17 @@ function getElementByIndexes(arr, indexes) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8 ]   =>  [ 5, 6, 7, 8, 1, 2, 3, 4 ]
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const newArr = [];
+  const length = Math.floor(arr.length / 2);
+  newArr.push(...arr.slice(-length));
+  if (arr.length > 1) {
+    if (arr.length % 2 !== 0) {
+      newArr.push(...arr.slice(length, length + 1));
+    }
+    newArr.push(...arr.slice(0, length));
+  }
+  return newArr;
 }
 
 
